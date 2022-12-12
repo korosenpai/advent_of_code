313c46121b1e648f1f9bef68d5ec4a0a6445f446
korosenpai <koro.dev404@gmail.com>
2022-12-12 11:25:07 +0100

    day 5 part 2

---
+++ 2022/day-5/2.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm> // for 'remove'
+#include <cstring> // for 'strtok'
+using namespace std;
+
+/*
+store inputs in two files ->
+    board.in -> board,
+    inp,in -> instructions
+*/
+
+
+/*
+architecture of board:
+{   {z, n}, {m, c, d}, {p}  }
+
+z, m, p are at the base
+
+to move elements -> slice list to move from from bottom and bottom - n crates to carry
+-> save to and array and remove elem from vector
+append sliced list to crate to append to
+*/
+vector<vector<string>> createBoard() {
+    vector<vector<string>> board = {};
+    int charPerCrate = 4;
+    int nCrates = 0;
+    string line;
+    
+    ifstream myfile ("board.in");
+    if (myfile.is_open()) {
+        while ( getline (myfile,line) ) {
+            line += " "; // add last space to make last char 4 spaces
+
+            // do not read last line
+            if (myfile.tellg() <= 1) continue;
+            
+            // get number of crates
+            if (nCrates == 0) {
+                nCrates = (line.length() + 1) / charPerCrate; // + 1 is to add last char " "
+                
+                for (int n = 0; n < nCrates; n++) board.push_back({}); // create empty lists in board
+
+            }
+
+            // three times and get 4 chars to then add to list
+            for (int n = 0; n < nCrates; n++) {
+                string crate = line.substr(n * charPerCrate, charPerCrate);
+                
+                // remove all spaces
+                crate.erase(remove(crate.begin(), crate.end(), ' '), crate.end());
+
+                // add crate to list
+                if (crate != "") {
+                    // cout << "---" << crate << "---" << endl;
+                    board[n].insert(board[n].begin(), crate);
+                }
+            }
+
+            // cout << line << endl;
+    
+        }
+        myfile.close();
+        return board;
+    }
+    // file not found
+    return {};
+}
+
+void printBoard(vector<vector<string>> board) {
+    for (int i = 0; i < board.size(); ++i) {
+        for (int l = 0; l < board[i].size(); ++l) {
+            cout << board[i][l] << endl;
+        }
+        cout << endl;
+    }
+}
+
+vector<string> split(string str, const char* sep) {
+    vector<string>returnedStr = {};
+	char *token = strtok(str.data(), sep);
+
+    // Keep printing tokens while one of the
+    // delimiters present in str[].
+    while (token != NULL)
+    {
+        // cout << "1--" << token << endl;
+        returnedStr.push_back(token);
+        token = strtok(NULL, sep);
+    }
+
+    return returnedStr;
+}
+
+
+int main() {
+    vector<vector<string>> board = createBoard();
+    string line;
+    
+    ifstream myfile ("inp.in");
+    if (myfile.is_open()) {
+        while ( getline (myfile,line) ) {
+    
+            vector<string> instruction = split(line, " ");
+
+            int nCratesToMove = stoi(instruction[1]);
+            int crateToMoveFrom = stoi(instruction[3]) - 1; // - 1 is for how it is stored board
+            int crateToMoveTo = stoi(instruction[5]) - 1;
+
+            // get sliced array to move to list
+            vector<string> cratesToMove(
+                board[crateToMoveFrom].end() - nCratesToMove, board[crateToMoveFrom].end()
+            );
+
+            // add sliced array to board
+            board[crateToMoveTo].insert(
+                board[crateToMoveTo].end(), cratesToMove.begin(), cratesToMove.end()
+            );
+
+            // remove n elements from bottom of board
+            board[crateToMoveFrom].erase(
+                board[crateToMoveFrom].end() - nCratesToMove, board[crateToMoveFrom].end()
+            );
+
+            // cout << line << endl;
+        }
+        myfile.close();
+    }
+    else { cout << "file not found" << endl; }
+
+    // printBoard(board);
+
+    // get final message
+    string finalMsg = "";
+    for (int i = 0; i < board.size(); ++i) {
+        finalMsg += board[i][board[i].size() - 1][1];
+    }
+    cout << finalMsg << endl;
+}
